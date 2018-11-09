0930ebbaa15299903ba1d86f28107a660da91250
dxq <qaz60437352@gmail.com>
2018-11-09 22:43:45 +0800

    剑来正则匹配新章节标签

--- RealTimeChecker.py
+++ RealTimeChecker.py
@@ -11,18 +11,18 @@ def main(args):
         email_send_list,
         '《剑来》',
         'http://book.zongheng.com/showchapter/672340.html',
-        '<a  href=\"http:\/\/book.zongheng.com\/chapter\/672340/(.*?)\" target=\"_blank\" title=\"(.*?)\">(.*?)<\/a>',
+        '<a.*?href=\"http:\/\/book.zongheng.com\/chapter\/672340/(.*?)\" target=\"_blank\" title=\"(.*?)\">(.*?)<\/a>',
         url_prefix='http://book.zongheng.com/chapter/672340/',
         tips='章',
     )
-    dadaochaotian = UpdateMonitorBaseClass(
-        email_send_list,
-        '《大道朝天》',
-        'https://book.qidian.com/info/1010496369#Catalog',
-        '<a href="\/\/vipreader.qidian.com\/chapter\/1010496369\/(.*?)\" .*?title=\"(.*?)\">(.*?)<\/a>',
-        url_prefix='http://vipreader.qidian.com/chapter/1010496369/',
-        tips='章',
-    )
+    # dadaochaotian = UpdateMonitorBaseClass(
+    #     email_send_list,
+    #     '《大道朝天》',
+    #     'https://book.qidian.com/info/1010496369#Catalog',
+    #     '<a href="\/\/vipreader.qidian.com\/chapter\/1010496369\/(.*?)\" .*?title=\"(.*?)\">(.*?)<\/a>',
+    #     url_prefix='http://vipreader.qidian.com/chapter/1010496369/',
+    #     tips='章',
+    # )
 
 
 if __name__ == '__main__':
